aeb4c65c5d6886663739a13ad711e001c8921332
bilibil31-lime <bilibil31.lemonlike@gmail.com>
2026-07-19 23:07:37 +0700

    Update pomelo_key.lua

--- Core/pomelo_key.lua
+++ Core/pomelo_key.lua
@@ -1,5 +1,15 @@
-KEY:JJK10101
-selece: linkvertisk (link:djgidjg)/ io.oi (link:lefkokef)
+KEY FOR LOGIN 
 ----------
+KEY FOR linkvertisk:JJK10101
+KEY FOR io.oi:JJK10101333
+selece: linkvertisk (link:djgidjg)/ io.oi (link:lefkokef)
+---
 ADMIN KEY:bilibil31Xnugget776
 friend key:GGboodXnugget775 [12]
+
+CODE CREDIT FOR BUY SCRIPT
+----------
+CODE1 = JDNGFSJFHGS
+CODE2 = KDFJJDFDF
+CODE3 = GOJSOJDGO
+
