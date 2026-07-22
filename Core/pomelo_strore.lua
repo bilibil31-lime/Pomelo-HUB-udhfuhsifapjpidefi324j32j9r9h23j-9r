fc27d3c499acbd7668e812d0689bbba65253f546
bilibil31-lime <bilibil31.lemonlike@gmail.com>
2026-07-22 20:20:49 +0700

    Update pomelo_strore.lua

--- Core/pomelo_strore.lua
+++ Core/pomelo_strore.lua
@@ -1,1 +1,10 @@
-
+-----------------
+script1 name=fkfkfkf hub
+=loadstring(game:HttpGet("https://pastebin.com/j9qaEFp0"))()
+Description=ifjihgjrijgifjigjif
+price=1
+-----------------
+script2 name=fkfkfkf hubdfjijd
+=loadstring(game:HttpGet("https://pastebin.com/j9qasdFp0"))()
+Description=ifjihgjrijsdfjigjif
+price=1
